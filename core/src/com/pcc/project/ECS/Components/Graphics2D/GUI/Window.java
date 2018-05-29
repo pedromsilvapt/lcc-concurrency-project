66a761b1f14605fc849646ca65c692be0207c6ae
Pedro M. Silva <pemiolsi@hotmail.com>
2018-05-29 10:58:57 +0100

    Small tweaks in the window component

--- core/src/com/pcc/project/ECS/Components/Graphics2D/GUI/Window.java
+++ core/src/com/pcc/project/ECS/Components/Graphics2D/GUI/Window.java
@@ -12,120 +12,131 @@ import com.pcc.project.Prefabs.GameObject;
 
 
 public class Window extends VisualComponent {
-    public static String windowName = "Vaga Vermelha";
+    public static String name = "window";
 
-    public enum WindowState {
-        Normal, Hovered, Pressed
-    }
-
-    protected WindowState state = WindowState.Normal;
+    protected Theme theme = Theme.Blue;
 
     protected Sprite spriteStateNormal;
-    protected Sprite spriteStateHovered;
-    protected Sprite spriteStatePressed;
-    
-    protected Entity buttonLabel;
+
+    protected Entity windowLabel;
 
     protected Text windowLabelText;
-    protected String value = "";
 
+    protected String title = "";
 
-    public Window(Entity entity, String name) {
-        super(entity, name);
+    public Window ( Entity entity, String name ) {
+        super( entity, name );
     }
 
-    public Window setAlign(int align){ 
-        if(this.align != align){
-            super.setAlign(align);
+    public Window setAlign ( int align ) {
+        if ( this.align != align ) {
+            super.setAlign( align );
         }
         return this;
     }
 
-    public String getValue () {
-        return this.value;
+    // Adding getters and setters for the window
+    public Theme getTheme () {
+        return this.theme;
     }
 
-    public Window setValue ( String value ) {
-        if ( this.value != value ) {
-            this.value = value;
+    public Window setTheme ( Theme theme ) {
+        this.theme = theme;
 
-            this.refreshLabel();
+        return this;
+    }
+
+    public String getTitle () {
+        return this.title;
+    }
+
+    public Window setTitle ( String value ) {
+        if ( this.title != value ) {
+            this.title = value;
+
+            this.onAlign();
         }
 
         return this;
     }
-    
-    public void onAlign(){
-        Vector2 anchor = this.getAnchorPosition();
 
-        this.buttonLabel.getComponent( Transform.class )
-                .setPosition( anchor.x + this.getSize().width/2, anchor.y + this.getSize().height/2);
+
+    public void onAlign () {
+        // We call this method when the text or size of the window changes, to refresh
+        // the position of the Title so that it is always centered and on the top
+
+        // But if we happen to change any of those things before the window was even created
+        // Then the pointer windowLabel would be null and using it would throw an exception
+        // So we don't run the function in those cases
+        if ( this.windowLabel == null ) {
+            return;
+        }
+
+        // We change to align code
+        // In OpenGL, the point (0,0) lays in the left bottom corner. So, incrementing the Y coordinate
+        // moves things up. Since we want the title of the window to be on top of it, we have to set the position
+        // to the height of the windows minus the height of the text
+        Vector2 anchor = this.getAnchor();
+
+        float x = anchor != null ? anchor.x : 0;
+        float y = anchor != null ? anchor.y : 0;
+
+        this.windowLabel.getComponent( Transform.class )
+                .setPosition( x + 10, y + this.getSize().height - 30 );
+
+        this.windowLabel.getComponent( Text.class )
+                .setValue( this.getTitle() )
+                .setSize( new Size( this.getSize().width - 20, 30 ) );
     }
 
-    public String getWindowAssetName ( Window.WindowState state ) {
-        return String.format( "grey_button05" );
+    public String getThemeString () {
+        return Theme.getString( this.theme );
+    }
+
+    public String getWindowAssetName () {
+        // We also changed this function to return the path for the window.
+        // Again, since windows don't need state, we removed that argument
+        // And added a property called theme that allows us to set the window color
+        // from the available images (blue, green, yellow, red)
+        return String.format( "metalPanel_%s", this.getThemeString() );
     }
 
 
     public void onAwake () {
         super.onAwake();
 
-        Sprite.PatchConfig buttonPatch = new Sprite.PatchConfig(12, 12, 23, 21);
+        // We  had to change the PatchConfig a little, the third value (top) was changed to 30
+        // Since that is the height of the window bar on the top
+        Sprite.PatchConfig windowPatch = new Sprite.PatchConfig( 12, 12, 30, 21 );
 
-        this.spriteStateNormal = this.entity.addComponent(Sprite.class, "window_normal");
+        // Unlike buttons or textboxes, our windows will only have one "state": normal
+        // Since buttons could be pressed and hovered with the mouse, they needed more than one sprite
+        // But one for windows will do just fine
+        this.spriteStateNormal = this.entity.addComponent( Sprite.class, "window_normal" );
         this.spriteStateNormal
-                .setNinePatchConfig(buttonPatch)
-                .setTexturePath(String.format("uipack/PNG/%s.png", this.getWindowAssetName(Window.WindowState.Normal)))
-                .setAlign(this.getAlign())
-                .setSize(this.size)
-                .setEnabled(this.state == Window.WindowState.Normal);
+                .setNinePatchConfig( windowPatch )
+                .setTexturePath( String.format( "uipack-space/PNG/%s.png", this.getWindowAssetName() ) )
+                .setAlign( this.getAlign() )
+                .setSize( this.size )
+                .setAnchor( this.getAnchor() );
+        // And since we only have one state, it is always enabled
+        //.setEnabled(this.state == Window.WindowState.Normal);
 
-        this.spriteStateHovered = this.entity.addComponent(Sprite.class, "window_hovered");
-        this.spriteStateHovered
-                .setNinePatchConfig(buttonPatch)
-                .setTexturePath(String.format("uipack/PNG/%s.png", this.getWindowAssetName(Window.WindowState.Hovered)))
-                .setAlign(this.getAlign())
-                .setSize(this.size)
-                .setEnabled(this.state == Window.WindowState.Hovered);
+        this.windowLabel = this.entity.instantiate( new GameObject( "windowLabel" ) );
 
-        this.spriteStatePressed = this.entity.addComponent(Sprite.class, "window_pressed");
-        this.spriteStatePressed
-                .setNinePatchConfig(buttonPatch)
-                .setTexturePath(String.format("uipack/PNG/%s.png", this.getWindowAssetName(Window.WindowState.Pressed)))
-                .setAlign(this.getAlign())
-                .setSize(this.size)
-                .setEnabled(this.state == Window.WindowState.Pressed);
-
-        this.buttonLabel = this.entity.instantiate(new GameObject("buttonLabel"));
-
-        this.onAlign();
-
-        this.windowLabelText = this.buttonLabel.addComponent( Text.class );
+        this.windowLabelText = this.windowLabel.addComponent( Text.class );
         this.windowLabelText
                 .setBitmapFont( "fonts/KenVector_Future_16_white.fnt" )
-                .setValue( this.getValue() )
+                .setValue( this.getTitle() )
                 .setAutoSize( false )
                 .setWrap( false )
-                .setTruncateText( "" )
-                .setColor( Color.BLACK )
-                .setTextAlign( Align.left )
-                .setAlign( Align.center )
-                .setSize( new Size( this.getSize().width - 35, this.getSize().height - 20 ) );
+                .setTruncateText( "..." )
+                .setColor( Color.WHITE )
+                .setTextAlign( Align.center )
+                .setSize( new Size( this.getSize().width - 20, 30 ) );
 
-        this.refreshLabel();
+        this.onAlign();
     }
-
-    protected void refreshLabel () {
-        if (this.windowLabelText == null) {
-            return;
-        }
-    }
-    
-    
-    
-    
-    
-    
 }
 
 
